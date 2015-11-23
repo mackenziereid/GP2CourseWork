73240235590e0705386ca7da7933dffe9c58eb57
jamesfglasgow <Jamesfglasgow@gmail.com>
2015-11-23 15:40:34 +0000

    MATERIAL created

---
+++ include/Material.h
@@ -0,0 +1,13 @@
+//
+//  Material.h
+//  GP2BaseCode
+//
+//  Created by james glasgow on 23/11/2015.
+//
+//
+
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+
+#endif /* Material_h */
---
+++ src/Material.cpp
@@ -0,0 +1,9 @@
+//
+//  Material.cpp
+//  GP2BaseCode
+//
+//  Created by james glasgow on 23/11/2015.
+//
+//
+
+#include "Material.h"
